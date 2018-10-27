d4ee9b40aa0dc0ee1f7742af54669ca4c6e8e541
Kevin519 <kpconway519@gmail.com>
2018-10-27 12:06:04 -0400

    pulling

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+
+var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+$.ajax({
+  url: url,
+  method: 'GET',
+}).done(function(result) {
+  console.log(result);
+}).fail(function(err) {
+  throw err;
+});
