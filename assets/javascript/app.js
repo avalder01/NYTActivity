0735eb677d3d5830d54f2792509c57484cec4202
Bingxi Du <bxdu@Bingxis-MacBook-Pro.local>
2018-10-27 12:26:27 -0400

    js added

---
+++ assets/javascript/app.js
@@ -0,0 +1,22 @@
+//creat varibles for search term, Number of record,..
+//data-name
+var searchTerm = $(this).attr("data-name")
+var numRec = $(this).attr("data-name")
+var start = $(this).attr("data-name")
+var end = $(this).attr("data-name")
+var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+url += '?' + $.param({
+  'api-key': "540d28fa74c0460b90258ab8b1561336",
+  'q': searchTerm,
+  'fq': numRec,
+  'begin_date': start,
+  'end_date': end
+});
+$.ajax({
+  url: url,
+  method: 'GET',
+}).done(function(result) {
+  console.log(result);
+}).fail(function(err) {
+  throw err;
+});
