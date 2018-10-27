0668717f12bfdea3ffbcc29397cd4ba1a9d7d583
Bingxi Du <bxdu@Bingxis-MacBook-Pro.local>
2018-10-27 18:02:13 -0400

    front ready

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,6 +7,24 @@
 
 //creat varibles for search term, Number of record,..
 //data-name
+var today = new Date(); 
+var mm = today.getMonth()+1; 
+var dd = today.getDate();
+
+var mmString;
+var ddString;
+
+if(mm<10){
+    mmString='0'+mm.toString();
+}
+
+if(dd<10){
+    ddString='0'+dd.toString();
+}
+
+ 
+console.log(mm);
+console.log(dd);
 
 window.onload = function() {
 
@@ -16,32 +34,77 @@ window.onload = function() {
   };
 
 function deleteF(){
-
+   $("#articles-here").empty();
 }
-function submitF(){
 
+
+function submitF(){
 var searchTerm = $("#search-term").val()
 console.log(searchTerm);
 var numRec = $("#num-to-retrieve").val()
 console.log(numRec);
-var start = $("#start-year").val()
-var end = $("#end-year").val()
+var startyear = $("#start-year").val()
+var endyear  = $("#end-year").val()
+var start;
+var end;
+
+if(startyear.length==4){
+    start = startyear+"0101"
+}else{
+    start ='';
+}
+
+if(endyear.length==4){
+    if(endyear <= 2017){
+        end = endyear + '1231'
+    }else{
+        end = endyear + mmString + ddString;
+    }
+    
+}else{
+    end = '';
+}
+
+
 var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-url += '?' + $.param({
-  'api-key': "540d28fa74c0460b90258ab8b1561336",
-  'q': searchTerm,
-  'fq': numRec,
-  'begin_date': start,
-  'end_date': end
-});
+
+if (start.length==4){
+    url += '?' + $.param({
+        'api-key': "540d28fa74c0460b90258ab8b1561336",
+        'q': searchTerm,
+        'fq': numRec,
+        'begin_date': start,
+        'end_date': end
+      });
+      console.log(url);
+}else{
+    url += '?' + $.param({
+        'api-key': "540d28fa74c0460b90258ab8b1561336",
+        'q': searchTerm,
+        'fq': numRec
+    });
+}
+
+
 $.ajax({
   url: url,
   method: 'GET',
 }).done(function(result) {
-  console.log(result);
-}).fail(function(err) {
-  throw err;
-});
+    //creat array called headline, holding the healines
+     var headlineHoder = [];
+     for(i=0;i<numRec;i++){
+         headlineHoder.push(result.response.docs[i].headline.main);
+         var headlineDiv = $("<a>").text(result.response.docs[i].headline.main);
+         headlineDiv.attr("href",result.response.docs[i].web_url);
+         headlineDiv.append("<br>");
+         headlineDiv.css("padding-top","10px");
+         console.log(result.response.docs[i].web_url);
+         $("#articles-here").append(headlineDiv);
+     }
+     
+     $("#articles-here").css("padding-top","20px")
+  
+})
 
 }
 
